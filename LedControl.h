27ae728579d382222920dddd0f7318ea7fe46465
Copelama <118077780+Copelama@users.noreply.github.com>
2024-04-14 13:31:49 -0400

    Update LedControl.h

--- LedControl.h
+++ LedControl.h
@@ -1,39 +1,11 @@
-/*
- *    LedControl.h - A library for controling Leds with a MAX7219/MAX7221
- *    Copyright (c) 2007 Eberhard Fahle
- * 
- *    Permission is hereby granted, free of charge, to any person
- *    obtaining a copy of this software and associated documentation
- *    files (the "Software"), to deal in the Software without
- *    restriction, including without limitation the rights to use,
- *    copy, modify, merge, publish, distribute, sublicense, and/or sell
- *    copies of the Software, and to permit persons to whom the
- *    Software is furnished to do so, subject to the following
- *    conditions:
- * 
- *    This permission notice shall be included in all copies or 
- *    substantial portions of the Software.
- * 
- *    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
- *    EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
- *    OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
- *    NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
- *    HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
- *    WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
- *    FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
- *    OTHER DEALINGS IN THE SOFTWARE.
- */
-
 #ifndef LedControl_h
 #define LedControl_h
 
 #include <avr/pgmspace.h>
+#include <avr/io.h>
+#include <avr/pgmspace.h>
 
-#if (ARDUINO >= 100)
-#include <Arduino.h>
-#else
 #include <WProgram.h>
-#endif
 
 /*
  * Segments to be switched on for characters and digits on
